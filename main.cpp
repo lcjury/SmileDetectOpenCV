1830cb00e713cb4bb8b891154a9db98903d5040f
Luis Jury <zeui16@gmail.com>
2015-05-13 00:59:14 -0500

    añadido dibujado de interfaz

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@ using namespace std;
 
 RNG rng(12345);
 
+
+void SimplestCB(Mat& in, Mat& out, float percent) {
+    assert(in.channels() == 3);
+    assert(percent > 0 && percent < 100);
+ 
+    float half_percent = percent / 200.0f;
+ 
+    vector<Mat> tmpsplit; split(in,tmpsplit);
+    for(int i=0;i<3;i++) {
+	    //find the low and high precentile values (based on the input percentile)
+	    Mat flat; tmpsplit[i].reshape(1,1).copyTo(flat);
+	    cv::sort(flat,flat,CV_SORT_EVERY_ROW + CV_SORT_ASCENDING);
+	    int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
+	    int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));
+
+	    //saturate below the low percentile and above the high percentile
+	    tmpsplit[i].setTo(lowval,tmpsplit[i] < lowval);
+	    tmpsplit[i].setTo(highval,tmpsplit[i] > highval);
+
+	    //scale the channel
+	    normalize(tmpsplit[i],tmpsplit[i],0,255,NORM_MINMAX);
+    }
+    merge(tmpsplit,out);
+}
+
+
 Mat getSkin( const Mat & input )
 {
 	int Y_MIN = 0, Y_MAX = 255, Cr_MIN = 133, Cr_MAX = 173, Cb_MIN = 77, Cb_MAX = 127;
@@ -90,6 +116,9 @@ main(int argc, const char* argv[])
 	while(1 )
 	{
 		capture >> frame ;
+		// Algoritmo de balance de colores, no necesariamente funciona ):
+		//SimplestCB(frame,frame,1);
+
 		skin = getSkin(frame);
 		//Convierte el frame a escala de grises
 		cvtColor(frame, grayFrame, CV_BGR2GRAY);
@@ -108,7 +137,7 @@ main(int argc, const char* argv[])
 		//Scalar color = Scalar( rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255));
 		//drawContours( grayFrame, contours, 3, color, 3, CV_AA, hierarchy, 0, Point() );
 		//}
-		//
+
 		//Histogram Equalization
 		equalizeHist(grayFrame, grayFrame);
 
